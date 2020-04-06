e0618def67c57d7c2b93f068a427125eecbcffff
Gongjs <1845553629@qq.com>
2020-04-06 22:48:54 +0800

    修改个人主页

--- pages/GRZX/user.vue
+++ pages/GRZX/user.vue
@@ -166,7 +166,7 @@
 							//loginType=1,泉运登录界面
 							//loginType=2,今点通登录界面
 							//loginType=3,武夷股份登录界面
-							url  : '/pages/GRZX/userLogin?loginType=1'
+							url  : '/pages/GRZX/userLogin?loginType=3'
 						}) 
 					},1500);
 				}else{
