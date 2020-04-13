4078577e86ef6c6206a975e64c9c48dd38fcddc0
傅洺钦 <32589407@qq.com>
2020-04-13 14:01:03 +0800

    修改支付

--- pages/LYFW/scenicSpotTickets/orderAdd.vue
+++ pages/LYFW/scenicSpotTickets/orderAdd.vue
@@ -269,19 +269,19 @@
 									//loginType=1,泉运登录界面
 									//loginType=2,今点通登录界面
 									//loginType=3,武夷股份登录界面
-									url  : '/pages/GRZX/userLogin?loginType=1'
+									url  : '../../GRZX/userLogin?loginType=1'
 								}) 
 							},500);
 						},
 						success() {
 							uni.navigateTo({
-								url: '/pages/GRZX/addPassenger?type=add',
+								url: '../../GRZX/addPassenger?type=add',
 							})
 						}
 					})
 				}else if(e==1){
 					uni.navigateTo({
-						url: '/pages/GRZX/passengerInfo?submitType=1',
+						url: '../../GRZX/passengerInfo?submitType=1',
 					})
 				}
 				
