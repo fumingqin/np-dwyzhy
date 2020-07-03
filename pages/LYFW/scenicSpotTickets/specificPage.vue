3978cef368fb3a6339d0fdfc2e24df121f885fd9
傅洺钦 <32589407@qq.com>
2020-07-03 12:04:12 +0800

    1

---
+++ pages/LYFW/scenicSpotTickets/specificPage.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<web-view src="http://m.wyschina.com/tickets/detail/?iscenicid=1&szscenicname=武夷山风景名胜区&topics="></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
