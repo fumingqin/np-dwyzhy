d9da803ae03ea40e08e48e527ceda28311dd5376
caiweida <2332268008@qq.com>
2020-09-14 10:18:06 +0800

    添加攻略页面

--- pages/TravelGuide/tr_strategyDetails.vue
+++ pages/TravelGuide/tr_strategyDetails.vue
@@ -2,127 +2,31 @@
 	<view>
 		<!-- 顶部轮播图（可点击进入相册并放大） -->
 		<swiper class="swi" circular autoplay>
-			<swiper-item class="swiItem" v-for="(item,index) in picList" :key="index" @click="goImgList">
-				<image :src="item.ticketImage" />
+			<swiper-item class="swiItem" @click="goImgList">
+				<image :src="information.imgUrl" />
 				<!-- <view class="view">{{imgnumber}}</view> -->
 			</swiper-item>
 		</swiper>
 
 		<!-- 标题、发布时间-->
 		<view class="titleClass">
-			<text class="title">{{titleClick.scenicName}}</text>
+			<text class="title">{{information.title}}</text>
 			<view class="dateCost">
-				<view class="date">{{titleClick.date}}&nbsp;&nbsp;{{titleClick.time}}<text class="cost1">￥<text class="cost2">{{titleClick.cost}}</text>元</text></view>
+				<view class="date">{{information.updatedTime}}&nbsp;&nbsp;浏览数:{{information.count}}</view>
 			</view>
-			<!-- <view class="grClass">
-				<image class="txImage" :src="titleClick.image" mode="aspectFill"></image>
-				<view class="grView">
-					<view class="name">{{titleClick.name}}<text class="ladelView" style="background-color: #0CA1DF;">官方</text></view>
-					<text class="number">电话：{{titleClick.number}}</text>
-				</view>
-				<text class="address">|&nbsp;&nbsp;&nbsp;{{titleClick.address}}</text>
-			</view> -->
 		</view>
 
-		<!-- 顶部滑动 -->
-		<view class="screen">
-			<view class="screenView">
-				<view class="screenText" :class="{current:type===0}" @click="tabClick(0)">
-					精彩活动
-				</view>
-				<view class="screenText" :class="{current:type===1}" @click="tabClick(1)">
-					旅游攻略
-				</view>
-				<view class="screenText" :class="{current:type===2}" @click="tabClick(2)">
-					美食美宿
-				</view>
-				<view class="screenText" :class="{current:type===3}" @click="tabClick(3)">
-					结伴同行
-				</view>
-			</view>
-		</view>
 
 		<!-- 景区介绍 -->
-		<view v-if="type==0">
+		<view>
 			<!-- 内容 -->
 			<view class="scenicspotContent">
 				<view class="content2">
-					<rich-text :nodes="information[0].content"></rich-text>
+					<rich-text :nodes="information.content"></rich-text>
 				</view>
 			</view>
 		</view>
 
-		<!-- 行程安排 -->
-		<view v-if="type==1">
-			<view class="scheduling" v-for="(item,index) in arrangeText" :key="index">
-				<view class="tripTitle">
-					<view class="circle" style="background-color: #D8F3FF;">
-						<text class="circleDay">D{{item.day}}</text>
-					</view>
-					<text class="title">{{item.title}}</text>
-				</view>
-				<view class="contentView">
-					<view class="hotel">
-						<image class="iconHotel" src="../../../static/LYFW/ouristRoute/SceneryTable/travelArrange/jiudian.png"></image>
-						<text class="hotelText">酒店:&nbsp;{{item.hotel}}</text>
-					</view>
-					<view class="hotel">
-						<image class="iconHotel" src="../../../static/LYFW/ouristRoute/SceneryTable/travelArrange/yongcan.png"></image>
-						<text class="hotelText">用餐:&nbsp;{{item.dinnerTime}}</text>
-					</view>
-					<view class="content" v-for="(item2,index2) in information" :key="index2">
-						<view class="informationTitle">
-							<view class="titleDian"></view>
-							<view class="title2">
-								<rich-text :nodes="item2.title"></rich-text>
-							</view>
-						</view>
-						<view class="informationTime">
-							<rich-text :nodes="item2.createdTime"></rich-text>
-						</view>
-						<view class="informationText">
-							<rich-text :nodes="item2.content"></rich-text>
-						</view>
-					</view>
-				</view>
-			</view>
-		</view>
-
-		<!-- 费用说明 -->
-		<view v-if="type==2">
-			<view class="description">
-				<view class="costDescription" v-for="(item,index) in costDescription" :key="index">
-					<text class="costTitle">{{item.title}}</text>
-					<view class="costText">
-						<rich-text :nodes="item.way"></rich-text>
-					</view>
-				</view>
-			</view>
-		</view>
-
-		<!-- 预定须知 -->
-		<view v-if="type==3">
-			<view class="preset">
-				<view class="presetNeeds">
-					<view class="reserve" v-for="(item,index) in reserve" :key="index">
-						<text class="reserveTitle">{{item.title}}</text>
-						<view class="reserveText">
-							<rich-text :nodes="item.way"></rich-text>
-						</view>
-					</view>
-				</view>
-			</view>
-		</view>
-		<!-- 底部 -->
-		<!-- <view class="footer">
-			<view class="footerPrice">
-				<image class="kfIcon" src="../../../static/LYFW/ouristRoute/SceneryTable/travelArrange/kefu.png" @click="godetail"></image>
-				<image class="zfIcon" src="../../../static/LYFW/ouristRoute/SceneryTable/travelArrange/zhuanfa.png" type="primary" open-type="share" @click="share"></image>
-			</view>
-			<view class="submitChange" @click="submit">
-				<text class="submit">立即预订</text>
-			</view>
-		</view> -->
 	</view>
 </template>
 
@@ -142,6 +46,7 @@
 				costDescription: [], //费用明细
 				reserve: [], //预定须知
 				reserveContent: '', //预定须知内容
+				id:'',
 			}
 		},
 
@@ -179,33 +84,33 @@
 				})
 			},
 
-			tabClick(e) {
-				if (e == 0) {
-					this.type = 0;
-				} else if (e == 1) {
-					this.type = 1;
-				} else if (e == 2) {
-					this.type = 2;
-				} else if (e == 3) {
-					this.type = 3;
-				}
+			// tabClick(e) {
+			// 	if (e == 0) {
+			// 		this.type = 0;
+			// 	} else if (e == 1) {
+			// 		this.type = 1;
+			// 	} else if (e == 2) {
+			// 		this.type = 2;
+			// 	} else if (e == 3) {
+			// 		this.type = 3;
+			// 	}
 
-			},
+			// },
 
 			getArticleInfo: function() {
 				var that = this;
-				uni.request({
-					url: 'http://218.67.107.93:9210/api/app/getInformationList?id=' + this.id,
-					method: "POST",
-					success: function(res) {
-						that.information = res.data.data;
-						for (var i = 0; i < res.data.data.length; i++) {
-							that.information[i].content = res.data.data[i].content.replace(/\<img/g,
-								'<img style="max-width:100%;height:auto;margin: 2px 0px;" ');
-						}
-						console.log(res)
-					}
-				});
+				// uni.request({
+				// 	url: 'http://218.67.107.93:9210/api/app/getInformationList?id=' + this.id,
+				// 	method: "POST",
+				// 	success: function(res) {
+				// 		that.information = res.data.data;
+				// 		for (var i = 0; i < res.data.data.length; i++) {
+				// 			that.information[i].content = res.data.data[i].content.replace(/\<img/g,
+				// 				'<img style="max-width:100%;height:auto;margin: 2px 0px;" ');
+				// 		}
+				// 		console.log(res)
+				// 	}
+				// });
 				
 				// uni.request({
 				// 	url: 'http://218.67.107.93:9210/api/app/get-strategy-list',
@@ -219,10 +124,14 @@
 					url: 'http://218.67.107.93:9210/api/app/get-strategy-detail',
 					method: "GET",
 					data:{
-						id:37
+						id:this.id
 					},
 					success: (res) => {
-						console.log('攻略', res)
+						// console.log('攻略', res)
+						that.information = res.data.data;
+						that.information.content = res.data.data.content.replace(/\<img/g,
+							'<img style="max-width:100%;height:auto;margin: 2px 0px;" ');
+						console.log('攻略', that.information)
 					}
 				});
 			},
